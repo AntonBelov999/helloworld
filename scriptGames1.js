7098ced420506105c7631512140115244c60c538
AntonBelov999 <antowa91@icloud.com>
2025-02-17 10:46:51 +0300

    hw5

--- scriptGames1.js
+++ scriptGames1.js
@@ -1,18 +1,25 @@
 
-    function game1() {
-        let userAnswer;
-        userAnswer = Number(prompt('Угадай число'));
-        const number = `Math.floor(Math.random() * 100) + 1`;
-        if (userAnswer === 56) {
-            alert('угадал');
-        } else if (userAnswer < 56) {
-            alert('заданное число больше');   
-        } else if (userAnswer > 56) {
+function game1() {
+    const number = Math.floor(Math.random() * 100) + 1;
+    console.log(number)
+    console.log(Number(number))
+    while (true) {
+        let userAnswer = prompt('Угадай число');
+        if (userAnswer === null) {
+            alert('игра остановлена');
+            break;
+        }
+        if (isNaN(userAnswer)) {
+            alert('только число');
+            continue;
+        }
+        if (userAnswer < Number(number)) {
+            alert('заданное число больше');
+        } else if (userAnswer > Number(number)) {
             alert('заданное число меньше');
         } else {
-            alert('не понимаю');
+            alert('угадал');
+            break;
         }
     }
-   
-
-
+}
